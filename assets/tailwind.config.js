bd8869e3dbb3780c269e7aa2af652ba820c22ede
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-12 12:55:36 +0900

    tailwindcssとAlpine JSの設定

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,18 +1,30 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
+const colors = require("tailwindcss/colors")
 const plugin = require("tailwindcss/plugin")
 
 module.exports = {
   content: [
     "./js/**/*.js",
     "../lib/*_web.ex",
-    "../lib/*_web/**/*.*ex"
+    "../lib/*_web/**/*.*ex",
+    "../deps/petal_components/**/*.*ex",
   ],
   theme: {
     extend: {
       colors: {
         brand: "#FD4F00",
+
+        primary: colors.blue,
+        secondary: colors.pink,
+        success: colors.green,
+        danger: colors.red,
+        warning: colors.yellow,
+        info: colors.sky,
+
+        // Options: slate, gray, zinc, neutral, stone
+        gray: colors.gray,
       }
     },
   },
